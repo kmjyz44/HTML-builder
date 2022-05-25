60ebfc34bc54373feff88e6f6ab5dd08f55d2bfc
kmjyz44 <kmjyz44@gmail.com>
2022-05-25 21:38:25 +0300

    fix: 01-read-file  add path.join

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const readableStream = fs.createReadStream('text.txt','utf-8');
+
+const readableStream = fs.createReadStream(path.join(__dirname,'text.txt'),'utf-8');
 readableStream.on('data', chunk => console.log(chunk));
 readableStream.on('error', error => console.log('error'));
