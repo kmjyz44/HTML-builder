3e1ce7d2b4b7a0ba5421984c0c941fd6446320d3
kmjyz44 <kmjyz44@gmail.com>
2022-05-27 21:26:32 +0300

    fix: file  02"

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,15 +1,36 @@
 const fs = require('fs');
 const path = require('path');
-const { stdin, stdout, exit } = process;
-const output = fs.createWriteStream('destination.txt');
-console.log('Введите текст:')
+const { stdin, stdout } = process;
 
-stdin.on('data', data => output.write(data))
+let text ='' ;
+fs.writeFile(
+  path.join(__dirname, 'newFile.txt'),
+  '',
+  (err) => {
+    if (err) throw err;
+    console.log('Файл был создан');
+    stdout.write('Напишите текст \n');
+  }
+);
+stdin.on('data', (data) => {
+  text = data.toString();
+  if (text.trim() == 'exit') {
+    process.exit();
+  }
+  fs.appendFile(
+    path.join(__dirname, 'newFile.txt'),
+    text,
+    (err) => {
+      if (err) throw err;
+      console.log('Файл был изменен');
+    }
+  );
 
-process.on('exit', () => stdout.write('Удачи!!!'));
 
-output.on('error', error => console.log('Error', error.message));
+});
 
+process.on('SIGINT', process.exit);
 
-
-
+process.on('exit', () => {
+  stdout.write('program end');
+});
