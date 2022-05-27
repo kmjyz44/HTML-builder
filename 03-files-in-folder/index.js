201690b0fae62aa2d4bdcafa37da93aa222c183c
kmjyz44 <kmjyz44@gmail.com>
2022-05-27 22:19:23 +0300

    fix: file  03"

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,22 +1,27 @@
+
 var fs = require('fs');
 var path = require('path');
 
-    
-   let files = fs.readdirSync(path.join(__dirname, 'secret-folder'));
-        files.forEach(element => {
-           
-           fs.stat(path.join(__dirname, 'secret-folder', element), function(err,stats){
-           if(err){
-            console.log(err);
-           }
-           if(!stats.isDirectory()){
-           console.log(path.basename(element, path.extname(element))+' - '+ path.extname(element)+' - '+stats.size);
-            }
-            else{
-                console.log(element+'  - Не является файлом')
-            }
-             })
+async function rd (){
+  await fs.readdir((path.join(__dirname, 'secret-folder')), (err,files) =>{
+    files.forEach(element => {
+      fs.stat(path.join(__dirname, 'secret-folder', element), function(err,stats){
+        if(err){
+          console.log(err);
+        }
+        if(!stats.isDirectory()){
+          console.log(path.basename(element, path.extname(element))+' - '+ path.extname(element)+' - '+stats.size);
+        }
+        else{
+          console.log(element+'  - Не является файлом');
+        }
+      });
             
-            });    
+    });    
+  });
+}  
+async function rune (){
+  await rd();
 
-        
+}
+rune();
