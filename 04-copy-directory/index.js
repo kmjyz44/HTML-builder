8816d4a1da04b48de2bcefb9b5d1a44f76fc8edb
kmjyz44 <kmjyz44@gmail.com>
2022-05-28 14:40:27 +0300

    fix: file  04"

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -16,18 +16,32 @@ async function mdir (){
 async function delfiles(){
   let fc = await fs.promises.readdir (path.join(__dirname,'files-copy'));
   for(let fd of fc){
-    await fs.promises.unlink(path.join(__dirname,'files-copy',fd));
-    console.log(fd);
+    if(path.extname(path.join(__dirname,'files',fd))){
+      await fs.promises.unlink(path.join(__dirname,'files-copy',fd));
+    }
+    else{
+      await fs.promises.rmdir (path.join(__dirname,'files-copy', fd));
+    }
   }
 }
 async function readd(){
   const files = await fs.promises.readdir (path.join(__dirname,'files'));  
-  for(let element of files){     
-    fs.promises.copyFile((path.join(__dirname,'files',element)), (path.join(__dirname,'files-copy',element))).then (function (err) {
-      if(err) throw err; // не удалось скопировать файл
-      console.log('Файл успешно скопирован');
+  for(let element of files){ 
+    if(path.extname(path.join(__dirname,'files',element))){
+      fs.promises.copyFile((path.join(__dirname,'files',element)), (path.join(__dirname,'files-copy',element))).then (function (err) {
+        if(err) throw err; // не удалось скопировать файл
+        console.log('Файл успешно скопирован');
 
-    });
+      });
+    }
+    else{
+      console.log(element);
+     
+      fs.mkdir(path.join(__dirname,'files-copy', element), { recursive: true }, err => {
+        if (err)
+          throw err;
+      });   
+    }  
   }
 }
 
